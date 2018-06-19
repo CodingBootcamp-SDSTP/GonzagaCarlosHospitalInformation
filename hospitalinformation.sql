abd2828380de552c2d9d3d9ee986c8f6c086101e
kaloidaboi <carlos.gonzaga@netgies.com>
2018-06-19 18:19:45 +0800

    modified sql-script

--- hospitalinformation.sql
+++ hospitalinformation.sql
@@ -1,4 +1,14 @@
-CREATE TABLE IF NOT EXISTS person (
+DROP TABLE IF EXISTS patient_medical_case;
+DROP TABLE IF EXISTS medical_case;
+DROP TABLE IF EXISTS patient_doctor;
+DROP TABLE IF EXISTS attending_doctor;
+DROP TABLE IF EXISTS doctor;
+DROP TABLE IF EXISTS doctor_type;
+DROP TABLE IF EXISTS confinement;
+DROP TABLE IF EXISTS room;
+DROP TABLE IF EXISTS person;
+
+CREATE TABLE person (
 	person_id INT NOT NULL AUTO_INCREMENT,
 	lastname VARCHAR(40) NOT NULL,
 	firstname VARCHAR(60) NOT NULL,
@@ -9,7 +19,7 @@ CREATE TABLE IF NOT EXISTS person (
 	PRIMARY KEY (person_id)
 );
 
-CREATE TABLE IF NOT EXISTS room (
+CREATE TABLE room (
 	room_number INT NOT NULL,
 	type ENUM('PRIVATE', 'WARD'),
 	bed_capacity INT NOT NULL,
@@ -17,7 +27,7 @@ CREATE TABLE IF NOT EXISTS room (
 	PRIMARY KEY (room_number)
 );
 
-CREATE TABLE IF NOT EXISTS patient_confinement (
+CREATE TABLE confinement (
 	confinement_id INT NOT NULL AUTO_INCREMENT,
 	person_id INT NOT NULL,
 	admission_date_time DATETIME NOT NULL,
@@ -28,37 +38,41 @@ CREATE TABLE IF NOT EXISTS patient_confinement (
 	FOREIGN KEY (room_number) REFERENCES room (room_number)
 );
 
-CREATE TABLE IF NOT EXISTS doctor_type (
+CREATE TABLE doctor_type (
 	type_id INT NOT NULL AUTO_INCREMENT,
 	description VARCHAR(40) NOT NULL,
 	PRIMARY KEY (type_id)
 );
 
-CREATE TABLE IF NOT EXISTS doctor (
+CREATE TABLE doctor (
 	license_number INT NOT NULL,
-	doctor_id INT NOT NULL,
+	person_id INT NOT NULL,
 	type_id INT NOT NULL,
 	PRIMARY KEY (license_number),
-	FOREIGN KEY (doctor_id) REFERENCES person (person_id),
+	FOREIGN KEY (person_id) REFERENCES person (person_id),
 	FOREIGN KEY (type_id) REFERENCES doctor_type (type_id)
 );
 
-CREATE TABLE IF NOT EXISTS patient_doctor (
+CREATE TABLE attending_doctor (
 	confinement_id INT NOT NULL,
-	doctor_id INT NOT NULL,
+	license_number INT NOT NULL,
+	PRIMARY KEY (confinement_id, license_number),
+	FOREIGN KEY (confinement_id) REFERENCES confinement (confinement_id),
+	FOREIGN KEY (license_number) REFERENCES doctor (license_number)
 );
 
-CREATE TABLE IF NOT EXISTS medical_case (
+CREATE TABLE medical_case (
 	medical_case_id INT AUTO_INCREMENT,
-	medical_case_code VARCHAR(6) NOT NULL,
+	code VARCHAR(6) NOT NULL,
 	description VARCHAR(60) NOT NULL,
 	PRIMARY KEY (medical_case_id),
-	CONSTRAINT UC_medical_case UNIQUE (medical_case_code)
+	CONSTRAINT UC_medical_case UNIQUE (code)
 );
 
-CREATE TABLE IF NOT EXISTS patient_medical_case (
+CREATE TABLE patient_medical_case (
 	confinement_id INT NOT NULL,
 	medical_case_id INT NOT NULL,
-	FOREIGN KEY (confinement_id) REFERENCES patient_confinement (confinement_id),
+	PRIMARY KEY (confinement_id, medical_case_id),
+	FOREIGN KEY (confinement_id) REFERENCES confinement (confinement_id),
 	FOREIGN KEY (medical_case_id) REFERENCES medical_case (medical_case_id)
 );
